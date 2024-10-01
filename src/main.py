288d4d9e2611cb46f01ef5c1c931aaf62ff51ae5
Hamsterpants <kevinhoejgaard@gmail.com>
2024-10-01 12:48:27 +0000

    change main to update db with issue id

--- src/main.py
+++ src/main.py
@@ -5,20 +5,21 @@ import json
 sys.path.insert(0, '/workspaces/TrackPoint/src')
 
 from database.setup import setup_database
-from database.manager import fetch_database
+from database.manager import DatabaseManager
 from github_epic import github_epic
 from secret_manager import access_secret_version
 
-spreadsheet_id = "1iC75ObLb5ZvJ4NedUmnA5O98XGVNYno0IeJYFg2DVZ8"     # ID of the current spreadsheet.
+spreadsheet_id = "1o5JoaPwq7uP9oYs9KuhFBc9MJP6JybIhaLRUscgame8"
 db_collection = "kevintest"
 db_document = "MVP for TrackPoint"
 owner = "TrackPointDev"
 repo = "TrackPointTest"
 
 def main():
+    db_manager = DatabaseManager()
     # Run setup only once to initialize the database if needed.
-    # setup_database(spreadsheet_id, db_collection)
-    data = fetch_database(db_collection, db_document)
+    setup_database(spreadsheet_id, db_collection, db_manager) 
+    data = db_manager.fetch_database(db_collection, db_document)
     #print(data)
     tasks_with_title = [task for task in data['tasks'] if task.get('title')]
 
@@ -28,6 +29,7 @@ def main():
     secret_id = "hamsterpants-github-pat"
     version_id = "latest"
     token = access_secret_version(project_id, secret_id, version_id)
+    print(token)
 
     epic_title = data['title']
     epic_problem = data['problem']
@@ -41,7 +43,19 @@ def main():
     print(_github_epic.get_tasks())
 
     #Outcommented to avoid creating issues
-    #_github_epic.create_github_issues()
+    print("Creating issues")
+
+    _github_epic.create_issues()
+
+    print(_github_epic.get_tasks())
+
+    input("Press enter to update DB")
+    db_manager.update_db(db_collection, db_document, _github_epic.get_epic())
+
+    input("Press enter to continue")
+    print("Deleting epic")
+    #db_manager.delete_epic(db_collection, db_document)
+
 
 if __name__ == "__main__":
     main()
