8b72801aa32e52a558271a618e73c6a12636deda
Hamsterpants <kevinhoejgaard@gmail.com>
2024-10-01 09:20:19 +0000

    change google init to use secretmanager

--- src/Google/__init__.py
+++ src/Google/__init__.py
@@ -3,6 +3,7 @@ import os.path
 from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
+from secret_manager import access_secret_version_json
 
 cache = {}
 
@@ -11,8 +12,12 @@ cred_path = os.path.join(base_dir, 'credentials.json')
 
 
 def authenticate_service():
-    """Authenticate the service account with Google Sheets API.
+    project_id = "trackpointdb"
+    secret_id = "credentials_json"
+    version_id = "latest"
+    credentials_json = access_secret_version_json(project_id, secret_id, version_id)
 
+    """Authenticate the service account with Google Sheets API.
     See docs: https://developers.google.com/sheets/api/quickstart/python"""
 
     if "credentials" in cache:
@@ -31,8 +36,8 @@ def authenticate_service():
         if creds and creds.expired and creds.refresh_token:
             creds.refresh(Request())
         else:
-            flow = InstalledAppFlow.from_client_secrets_file(
-                cred_path, scopes
+            flow = InstalledAppFlow.from_client_config(
+                credentials_json, scopes
             )
             creds = flow.run_local_server(port=0)
         with open("token.json", "w") as token:
