ae3bd03f5076a1ca82fa70f8d2398609e060f37b
Hamsterpants <kevinhoejgaard@gmail.com>
2024-10-01 15:12:52 +0000

    improve readability in main

--- src/main.py
+++ src/main.py
@@ -1,60 +1,53 @@
-import sys
-import json
-  
-# adding src to the system path
-sys.path.insert(0, '/workspaces/TrackPoint/src')
-
 from database.setup import setup_database
 from database.manager import DatabaseManager
 from github_epic import github_epic
 from secret_manager import access_secret_version
 
-spreadsheet_id = "1o5JoaPwq7uP9oYs9KuhFBc9MJP6JybIhaLRUscgame8"
-db_collection = "kevintest"
-db_document = "MVP for TrackPoint"
-owner = "TrackPointDev"
-repo = "TrackPointTest"
+#TODO create a config or env file for these
+class Config:
+    def __init__(self):
+        self.spreadsheet_id = "1o5JoaPwq7uP9oYs9KuhFBc9MJP6JybIhaLRUscgame8"
+        self.db_collection = "kevintest"
+        self.db_document = "MVP for TrackPoint"
+        self.owner = "TrackPointDev"
+        self.repo = "TrackPointTest"
+        self.project_id = "trackpointdb"
+        self.secret_id = "hamsterpants-github-pat"
+        self.version_id = "latest"
 
 def main():
-    db_manager = DatabaseManager()
-    # Run setup only once to initialize the database if needed.
-    setup_database(spreadsheet_id, db_collection, db_manager) 
-    data = db_manager.fetch_database(db_collection, db_document)
-    #print(data)
-    tasks_with_title = [task for task in data['tasks'] if task.get('title')]
+    config = Config()
+    db_manager = DatabaseManager(config.db_collection, config.db_document)
 
-    # Create an instance of the github_epic class should maybe be moved to the database manager or smth
-    #small change
-    project_id = "trackpointdb"
-    secret_id = "hamsterpants-github-pat"
-    version_id = "latest"
-    token = access_secret_version(project_id, secret_id, version_id)
-
-    epic_title = data['title']
-    epic_problem = data['problem']
-    epic_feature = data['feature']
-    epic_value = data['value']
-
-    _github_epic = github_epic(owner, repo, token, epic_title, epic_problem, epic_feature, epic_value)
-    for task in tasks_with_title:
-        _github_epic.add_task(task)
+    setup_database(config.spreadsheet_id, db_manager)
     
-    print(_github_epic.get_tasks())
+    epic = db_manager.fetch_database()
 
-    #Outcommented to avoid creating issues
+    token = access_secret_version(config.project_id, config.secret_id, config.version_id)
+
+    gh_epic = github_epic(config.owner, config.repo, token, epic['title'], epic['problem'], epic['feature'], epic['value'])
+    
+    for task in epic['tasks']:
+        gh_epic.add_task(task)
+
     print("Creating issues")
+    gh_epic.create_issues()
 
-    _github_epic.create_issues()
-
-    print(_github_epic.get_tasks())
+    print("Getting tasks")
+    print(gh_epic.get_tasks())
 
     input("Press enter to update DB")
-    db_manager.update_db(db_collection, db_document, _github_epic.get_epic())
+    db_manager.update_db(gh_epic.get_epic())
+
+    taskwithid = db_manager.get_task_with_id(70)
+    print("f: Task with id: ", taskwithid)
+
+    taskwithtitle = db_manager.get_task_with_title("TEST TEST TEST")
+    print("f: Task with title: ", taskwithtitle)
 
     input("Press enter to continue")
     print("Deleting epic")
-    #db_manager.delete_epic(db_collection, db_document)
-
+    db_manager.delete_epic()
 
 if __name__ == "__main__":
     main()
