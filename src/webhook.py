53a35f0a78d6bddda30da8bdca65e666454167cd
Jacob F. Aslan-Lassen <s215528@dtu.dk>
2024-10-07 12:46:38 +0000

    Initial implementation of updating db with changed values from Github

--- src/webhook.py
+++ src/webhook.py
@@ -5,7 +5,11 @@ import json
 import uvicorn
 from fastapi import FastAPI
 from secret_manager import access_secret_version
+from database.manager import fetch_database, update_db
 from fastapi import Request
+
+db_collection = "epics"
+db_document = "MVP for TrackPoint"
 
 # Initialize FastAPI application
 app = FastAPI()
@@ -15,6 +19,31 @@ app = FastAPI()
 async def read_webhook(request: Request) -> dict:
     payload = await request.json()
     print(json.dumps(payload, indent=4))
+
+    if payload.get('action') == 'edited':
+        changes = payload.get('changes', {})
+        issue = payload.get('issue', {})
+
+        update_data = {}
+        for key in changes.keys():
+            print(f"Change detected in: {key}")
+            if key in issue:
+                update_data[key] = issue[key]
+        
+        #Update Firestore
+        issue_title = issue.get('title')
+        data = fetch_database(db_collection, db_document)
+        if update_data and issue_title:
+            update_db(db_collection, db_document, {str(issue_title): update_data})
+        
+        return {"status": "success", "value updated:": update_data}
+        
+        
+        
+
+    
+
+
     return payload
 
 def init_webhook():
